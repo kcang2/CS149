53c30acdb3a88b6d4fe32271c2bab5874caf38b4
kcang2 <42316301+kcang2@users.noreply.github.com>
2019-08-23 18:42:54 +0800

    Add files via upload

---
+++ asst1/prog1_mandelbrot_threads/mandelbrotThread.cpp
@@ -0,0 +1,132 @@
+#include <stdio.h>
+#include <pthread.h>
+
+#include "CycleTimer.h"
+
+typedef struct {
+    float x0, x1;
+    float y0, y1;
+    unsigned int width;
+    unsigned int height;
+    int maxIterations;
+    int* output;
+    int threadId;
+    int numThreads;
+} WorkerArgs;
+
+
+extern void mandelbrotSerial(
+    float x0, float y0, float x1, float y1,
+    int width, int height,
+    int startRow, int numRows,
+    int maxIterations,
+    int output[]);
+
+
+//
+// workerThreadStart --
+//
+// Thread entrypoint.
+void* workerThreadStart(void* threadArgs) {
+    double startTime = CycleTimer::currentSeconds();
+    WorkerArgs* args = static_cast<WorkerArgs*>(threadArgs);
+    
+    // TODO FOR CS149 STUDENTS: Implement the body of the worker
+    // thread here. Each thread should make a call to mandelbrotSerial()
+    // to compute a part of the output image.  For example, in a
+    // program that uses two threads, thread 0 could compute the top
+    // half of the image and thread 1 could compute the bottom half.
+
+    printf("Hello world from thread %d\n", args->threadId);
+//    int partition = (args->height)/(args->numThreads);
+//    int numRows;
+//    if ((args->threadId+1) == args->numThreads){
+//	numRows = (args->height)-(args->threadId)*partition;
+//    }
+//    else{
+//        numRows= partition;
+//    }
+//	
+//    mandelbrotSerial(args->x0, args->y0, args->x1, args->y1, args->width,
+//	 args->height, (args->threadId)*partition, numRows,
+//	 args->maxIterations, args->output);
+    
+    int tid = args->threadId;
+    int n = args->numThreads;
+    int startRow;
+    int numRows;
+    if (tid == 0){
+        startRow=0;
+        numRows=600-int(0.2675*400);
+    }
+    if (tid == 1){
+        startRow=600-int(0.2675*400);
+        numRows=2*int(0.2675*400);
+    }
+    if (tid==2){
+        startRow=600+int(0.2675*400);
+        numRows=args->height-startRow;
+    }
+    mandelbrotSerial(args->x0, args->y0, args->x1, args->y1, args->width,
+       args->height, startRow, numRows,
+       args->maxIterations, args->output);
+ 
+    double endTime = CycleTimer::currentSeconds();
+    printf("Thread: [%i]\t[%.3f] ms\n", args->threadId, (endTime - startTime)*1000);
+    return NULL;
+}
+
+//
+// MandelbrotThread --
+//
+// Multi-threaded implementation of mandelbrot set image generation.
+// Threads of execution are created by spawning pthreads.
+void mandelbrotThread(
+    int numThreads,
+    float x0, float y0, float x1, float y1,
+    int width, int height,
+    int maxIterations, int output[])
+{
+    const static int MAX_THREADS = 32;
+
+    if (numThreads > MAX_THREADS)
+    {
+        fprintf(stderr, "Error: Max allowed threads is %d\n", MAX_THREADS);
+        exit(1);
+    }
+
+    pthread_t workers[MAX_THREADS];
+    WorkerArgs args[MAX_THREADS];
+
+    for (int i=0; i<numThreads; i++) {
+      
+        // TODO FOR CS149 STUDENTS: You may or may not wish to modify
+        // the per-thread arguments here.  The code below copies the
+        // same arguments for each thread
+        args[i].x0 = x0;
+        args[i].y0 = y0;
+        args[i].x1 = x1;
+        args[i].y1 = y1;
+        args[i].width = width;
+        args[i].height = height;
+        args[i].maxIterations = maxIterations;
+        args[i].numThreads = numThreads;
+        args[i].output = output;
+      
+        args[i].threadId = i;
+    }
+
+    // Spawn the worker threads.  Note that only numThreads-1 pthreads
+    // are created and the main application thread is used as a worker
+    // as well.
+
+    for (int i=1; i<numThreads; i++)
+        pthread_create(&workers[i], NULL, workerThreadStart, &args[i]);
+    
+    workerThreadStart(&args[0]);
+
+    // wait for worker threads to complete
+    for (int i=1; i<numThreads; i++)
+        pthread_join(workers[i], NULL);
+}
+
